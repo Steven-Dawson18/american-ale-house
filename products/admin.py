671eef7166de8ea246241b1506031b0d5d519ea6
Steve <la14b@yahoo.co.uk>
2022-03-16 12:32:39 +0000

    Add product and category model and create some products in admin panel

---
+++ products/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+
+from .models import Product, Category
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'description',
+        'price',
+        'image',
+        'added_date',
+    )
+
+    ordering = ('added_date',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
