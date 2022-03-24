7ba942d14caa51ba6ad91a5fedf68fde5e515d76
Steve <la14b@yahoo.co.uk>
2022-03-24 16:25:46 +0000

    Added FAQ's view, template and url

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def faqs(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/faqs.html')
