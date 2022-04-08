a1d4b3b5854a4917055da4e9400b40c8375163ea
Steve <la14b@yahoo.co.uk>
2022-04-08 12:38:12 +0000

    Added profiles models and views tests

---
+++ favourites/test_favourites_models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from favourites.models import Favourites
+from products.models import Product
+
+
+class TestFavouriteModels(TestCase):
+    """
+    Testing for favourites models
+    """
+    def setUp(self):
+        """
+        Create test users, product and favourite
+        """
+        test_user = User.objects.create_user(
+            username='test_user', password='test_password')
+
+        product = Product.objects.create(
+            name='Test Product Name',
+            price='16.99',
+            description='Test Description',
+            abv='4.0',
+        )
+
+        Favourites.objects.create(
+            username=test_user
+        )
+
+    def test_favourites_str_method(self):
+        """
+        This test tests the favourites str method
+        """
+        favourite = Favourites.objects.get()
+        self.assertEqual((favourite.__str__()), "test_user's Favourites")
